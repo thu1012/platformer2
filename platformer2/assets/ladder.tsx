0ffad9c1ede108df5433b5ed82e498746a519bea
thu1012 <40744575+thu1012@users.noreply.github.com>
2018-07-05 16:08:48 -0400

    Add files via upload

---
+++ platformer2/assets/ladder.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ladder" tilewidth="69" tileheight="69" tilecount="1" columns="1">
+ <image source="ladder.png" width="69" height="69"/>
+</tileset>
